b8c85e046b9576116bb6f90eae1c9cd8dcb7ee93
Koen van den Heuvel <koenvandeheuvel@gmail.com>
2013-05-13 09:41:28 +0200

    Added mysql configuration and connection object

--- site/config.php
+++ site/config.php
@@ -1,5 +1,14 @@
 <?php 
 
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'supersoaker';
 
+$connection = mysqli_connect($hostname,$username,$password,$database);
+
+if (!$connection) {
+	die(mysqli_connect_error());
+}
 
 ?>
