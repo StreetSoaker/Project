580df472f42c6092519b8fa70f01c1347c4493cc
Robin Valk <valk3@hotmail.com>
2013-05-16 14:39:15 +0200

    Added form

--- site/pages/paswordrecovery.php
+++ site/pages/paswordrecovery.php
@@ -15,14 +15,24 @@ if (isset($_GET'token')) {
 
 	if ($stmt->num_rows == 1) {
 		$stmt->bind_result($userid, $startime);
-		if ($startime - mktime() > 0) {
+		if ((mktime() - $startime) <= 3600) {
+		?>
 
+			<form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+				<input type="password" name="pass1" />
+				<input type="password" name="pass1" />
+				<input type="submit" name="submit" />
+			</form>
+
+		<?php
+		} else {
+			$error[] = 'Password recovery token has expired';
 		}
 
 		$userSalt = str_pad(rand(0,9999999999), 10, '0', STR_PAD_LEFT);
 
 	} else {
-		$error['Invalid password recovery token']
+		$error[] = 'Invalid password recovery token';
 	}
 
 	$stmt->close();
@@ -36,7 +46,7 @@ if (isset($_POST['email']) && count($error) == 0) {
 
 	$email = $_POST['email'];
 
-	$stmt = $mysqli->prepare("SELECT `email`, `................................000000000000000000000000000000000000000000000000000000000000 FROM `users` WHERE `username` = ? LIMIT 0,1") or die($mysqli->error);
+	$stmt = $mysqli->prepare("SELECT `email` FROM `users` WHERE `username` = ? LIMIT 0,1") or die($mysqli->error);
 	$stmt->bind_param('s', $username);
 	$stmt->execute();
 	$stmt->store_result();
