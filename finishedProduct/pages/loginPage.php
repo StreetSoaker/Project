5477eb1b400058e70cce1502150f2bbf275d407c
Robin Valk <valk3@hotmail.com>
2013-06-05 14:03:30 +0200

    Started finishing

---
+++ finishedProduct/pages/loginPage.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Css -->
+    <link href="css/bootstrap.min.css"          rel="stylesheet" />
+    <!--<link href="css/bootstrap-responsive.css"   rel="stylesheet" />-->
+    <link href="css/normalize.css"              rel="stylesheet" />
+    <link href="css/core.css"                   rel="stylesheet" />
+    <!-- Javascript -->
+    <script src="js/jquery-1.9.1.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready( function() {            
+            function vcenter(){
+                var win_top = $(window).height() / 2;
+                var logo_top = $('.logo').height() / 2;
+                var login_top = $('.inputbox').height() / 2;
+                
+                var login_mid = win_top - login_top;
+                var logo_mid = win_top - logo_top;
+                
+                $('.logo').css({'top' : logo_mid+'px'});
+                $('.inputbox').css({'top' : login_mid+'px'});
+            }
+            
+            $('img').load(function(){
+                vcenter();
+                
+                $(window).resize(function(){
+                    vcenter();    
+                });
+            });
+
+
+            /*
+             * Login function
+             */
+            $('#login').submit(function(event) {
+                event.preventDefault();
+                var username = $('#username').val();
+                var password = $('#password').val();
+
+                $.post("pages/login.php", { 'username': username, 'password': password },
+                    function(data) {
+                        if(data == 1) {
+                            window.location = 'index.php';
+
+                            event.preventDefault();
+                        } else {
+                            $('body').append(data);
+                        }
+                    }
+                );
+            });
+        });
+    </script>
+</head>
+<body>
+    <!-- Login Screen -->
+    <div class="container-fluid" id="loginPage">
+        <div class="row-fluid">
+            <div class="span4 logo">
+                    <img class="offset1 loginImg" src="img/loginpage/login_logo.png" alt="StreetSoaker Logo" />
+            </div>
+            <div class="offset1 span7 inputbox">
+                    <form id="login">
+                        <input type="text" class="span9" placeholder="username/email" id="username" />
+                        <input type="submit" class="btn btn-large btn-primary loginButton pull-right" value="Login" /></br>
+                        <input type="password" class="span9" placeholder="password" id="password"/> 
+                    </form>
+                    <ul>
+                        <li><a href="#">Forgot password</a></li>
+                        <li><a href="#">Register account</a></li>
+                    </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
